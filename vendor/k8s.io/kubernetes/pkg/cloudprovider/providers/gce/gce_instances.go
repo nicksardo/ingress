8dc721d079b8d50a26ca46fa1753cd9f8cbb80b5
Nick Sardo <nicksardo@google.com>
2017-05-04 17:57:20 -0700

    get stats

--- vendor/k8s.io/kubernetes/pkg/cloudprovider/providers/gce/gce_instances.go
+++ vendor/k8s.io/kubernetes/pkg/cloudprovider/providers/gce/gce_instances.go
@@ -35,10 +35,10 @@ import (
 	"k8s.io/kubernetes/pkg/cloudprovider"
 )
 
-func newInstancesMetricContext(request string) *metricContext {
+func newInstancesMetricContext(request, zone string) *metricContext {
 	return &metricContext{
 		start:      time.Now(),
-		attributes: []string{"instances_" + request, unusedMetricLabel, unusedMetricLabel},
+		attributes: []string{"instances_" + request, unusedMetricLabel, zone},
 	}
 }
 
@@ -163,7 +163,7 @@ func (gce *GCECloud) AddSSHKeyToAllInstances(user string, keyData []byte) error 
 				})
 		}
 
-		mc := newInstancesMetricContext("add_ssh_key")
+		mc := newInstancesMetricContext("add_ssh_key", "")
 		op, err := gce.service.Projects.SetCommonInstanceMetadata(
 			gce.projectID, project.CommonInstanceMetadata).Do()
 
@@ -196,6 +196,7 @@ func (gce *GCECloud) GetAllZones() (sets.String, error) {
 
 	// TODO: Parallelize, although O(zones) so not too bad (N <= 3 typically)
 	for _, zone := range gce.managedZones {
+		mc := newInstancesMetricContext("list_zone", zone)
 		// We only retrieve one page in each zone - we only care about existence
 		listCall := gce.service.Instances.List(gce.projectID, zone)
 
@@ -215,7 +216,7 @@ func (gce *GCECloud) GetAllZones() (sets.String, error) {
 
 		res, err := listCall.Do()
 		if err != nil {
-			return nil, err
+			return nil, mc.Observe(err)
 		}
 		if len(res.Items) != 0 {
 			zones.Insert(zone)
@@ -312,7 +313,9 @@ func (gce *GCECloud) getInstanceByName(name string) (*gceInstance, error) {
 	// Avoid changing behaviour when not managing multiple zones
 	for _, zone := range gce.managedZones {
 		name = canonicalizeInstanceName(name)
+		mc := newInstancesMetricContext("get", zone)
 		res, err := gce.service.Instances.Get(gce.projectID, zone, name).Do()
+		mc.Observe(err)
 		if err != nil {
 			glog.Errorf("getInstanceByName: failed to get instance %s; err: %v", name, err)
 
